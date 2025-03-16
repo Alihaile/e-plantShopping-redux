4f3eee777ff51ca3c9f498098957411e3803966c
alihaile <alihaile2020@gmail.com>
2025-03-16 08:18:03 -0400

    added count

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,11 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 
+
 function ProductList({ onHomeClick }) {
+    
+    const cartItems = useSelector(state => state.cart.items);
+
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
@@ -238,6 +242,14 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    const cartIconStyle = {
+        display: 'inline',
+        position: 'fixed',
+        top: '33px',
+        fontSize: '24px',
+        right: '41px',
+    }
+
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -266,6 +278,15 @@ function ProductList({ onHomeClick }) {
         }))
     };
 
+    const getTotalCartItemsCount = () => {
+        var totalItems = 0;
+        cartItems.forEach((item) => {
+            totalItems += item.quantity 
+        });
+
+        return totalItems;
+    };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -283,7 +304,9 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <span style={cartIconStyle}>{ getTotalCartItemsCount() }</span>
+                        <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
